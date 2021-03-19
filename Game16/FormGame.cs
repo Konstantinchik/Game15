98edbd09568d109f8772d0dddbb707361baad25c
Konstantinchik <kgluschenko83@gmail.com>
2021-03-19 22:38:08 +0300

    Game deploy
    Game deploy

---
+++ Game16/FormGame.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game16
+{
+    public partial class FormGame15 : Form
+    {
+
+        Game game;
+
+        public FormGame15()
+        {
+            InitializeComponent();
+            game = new Game(4);
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            int position = Convert.ToInt16(((Button)sender).Tag);
+            game.shift(position);
+            refresh();
+            if (game.checkFinish()) MessageBox.Show("Вы победили!");
+            //button(position).Text = position.ToString();
+            //MessageBox.Show(position.ToString());
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            int position = Convert.ToInt16(((Button)sender).Tag);
+            game.shift(position);
+            refresh();
+            if (game.checkFinish()) MessageBox.Show("Вы победили!");
+            //button(position).Text = position.ToString();
+            //MessageBox.Show(position.ToString());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int position = Convert.ToInt16(((Button)sender).Tag);
+            game.shift(position);
+            refresh();
+            if (game.checkFinish()) MessageBox.Show("Вы победили!");
+            //button(position).Text = position.ToString();
+            //MessageBox.Show(position.ToString());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int position = Convert.ToInt16(((Button)sender).Tag);
+            game.shift(position);
+            refresh();
+            if (game.checkFinish()) MessageBox.Show("Вы победили!");
+            //button(position).Text = position.ToString();
+            //MessageBox.Show(position.ToString());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int position = Convert.ToInt16(((Button)sender).Tag);
+            game.shift(position);
+            refresh();
+            if (game.checkFinish()) MessageBox.Show("Вы победили!");
+            //button(position).Text = position.ToString();
+            //MessageBox.Show(position.ToString());
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int position = Convert.ToInt16(((Button)sender).Tag);
+            game.shift(position);
+            refresh();
+            if (game.checkFinish()) MessageBox.Show("Вы победили!");
+            //button(position).Text = position.ToString();
+            //MessageBox.Show(position.ToString());
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int position = Convert.ToInt16(((Button)sender).Tag);
+            game.shift(position);
+            refresh();
+            if (game.checkFinish()) MessageBox.Show("Вы победили!");
+            //button(position).Text = position.ToString();
+            //MessageBox.Show(position.ToString());
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int position = Convert.ToInt16(((Button)sender).Tag);
+            game.shift(position);
+            refresh();
+            if (game.checkFinish()) MessageBox.Show("Вы победили!");
+            //button(position).Text = position.ToString();
+            //MessageBox.Show(position.ToString());
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            int position = Convert.ToInt16(((Button)sender).Tag);
+            game.shift(position);
+            refresh();
+            if (game.checkFinish()) MessageBox.Show("Вы победили!");
+            //button(position).Text = position.ToString();
+            //MessageBox.Show(position.ToString());
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            int position = Convert.ToInt16(((Button)sender).Tag);
+            game.shift(position);
+            refresh();
+            if (game.checkFinish()) MessageBox.Show("Вы победили!");
+            //button(position).Text = position.ToString();
+            //MessageBox.Show(position.ToString());
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            int position = Convert.ToInt16(((Button)sender).Tag);
+            game.shift(position);
+            refresh();
+            if (game.checkFinish()) MessageBox.Show("Вы победили!");
+            //button(position).Text = position.ToString();
+            //MessageBox.Show(position.ToString());
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            int position = Convert.ToInt16(((Button)sender).Tag);
+            game.shift(position);
+            refresh();
+            if (game.checkFinish()) MessageBox.Show("Вы победили!");
+            //button(position).Text = position.ToString();
+            //MessageBox.Show(position.ToString());
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+
+        {
+            int position = Convert.ToInt16(((Button)sender).Tag);
+            game.shift(position);
+            refresh();
+            if (game.checkFinish()) MessageBox.Show("Вы победили!");
+            //button(position).Text = position.ToString();
+            //MessageBox.Show(position.ToString());
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            int position = Convert.ToInt16(((Button)sender).Tag);
+            game.shift(position);
+            refresh();
+            if (game.checkFinish()) MessageBox.Show("Вы победили!");
+            //button(position).Text = position.ToString();
+            //MessageBox.Show(position.ToString());
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            int position = Convert.ToInt16(((Button)sender).Tag);
+            game.shift(position);
+            refresh();
+            if (game.checkFinish()) MessageBox.Show("Вы победили!");
+            //utton(position).Text = position.ToString();
+            //MessageBox.Show(position.ToString());
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            int position = Convert.ToInt16(((Button)sender).Tag);
+            game.shift(position);
+            refresh();
+            if (game.checkFinish()) MessageBox.Show("Вы победили!");
+            //button(position).Text = position.ToString();
+            //MessageBox.Show(position.ToString());
+        }
+
+        //Receve location
+        private Button button (int position)
+        {
+            switch (position)
+            {
+                case 0: return button0;
+                case 1: return button1;
+                case 2: return button2;
+                case 3: return button3;
+                case 4: return button4;
+                case 5: return button5;
+                case 6: return button6;
+                case 7: return button7;
+                case 8: return button8;
+                case 9: return button9;
+                case 10: return button10;
+                case 11: return button11;
+                case 12: return button12;
+                case 13: return button13;
+                case 14: return button14;
+                case 15: return button15;
+                default: return null;
+            }
+
+        }
+
+        private void menu_start_Click(object sender, EventArgs e)
+        {
+            start_game();
+        }
+
+        private void start_game()
+        {
+            game.start();
+
+            //Shuffle numbers
+            for (int j = 0; j < 1000; j++)
+                game.Shuffle();
+
+            refresh();
+        }
+
+        private void refresh()
+        {
+            int len = 16;
+            for (int position = 0; position < len ; position++)
+            {
+                button(position).Text = game.get_number(position).ToString();
+                button(position).Visible = ((game.get_number(position)) > 0);
+            }
+                
+        }
+
+        private void FormGame15_Load(object sender, EventArgs e)
+        {
+            start_game();
+        }
+    }
+}
---
+++ Game16/Game.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game16
+{
+    class Game
+    {
+        int size;
+        int[,] map;
+        int space_x, space_y; // Coordinate of Empty
+        static Random rand = new Random();
+
+        public Game(int size)
+        {
+            if (size < 2) size = 2;
+            if (size > 5) size = 5;
+            this.size = size;
+            map = new int [size, size];
+        }
+
+        public void start()
+        {
+            for (int x = 0; x < (size); x++)
+                for (int y = 0; y < (size); y++)
+                    map[x, y] = CoordToPosition(x, y) + 1; // COORD 0, 0 = POS 1
+
+            space_x = size -1;
+            space_y = size -1;
+            map[space_x, space_y] = 0; // Empty place;
+        }
+
+        public int get_number(int position)
+        {
+            int x, y;
+            PositionToCoord(position, out x, out y);
+            if (x < 0 || x >= size) return 0;
+            if (y < 0 || y >= size) return 0;
+                        
+            return map[x, y];
+        }
+
+        private int CoordToPosition(int x, int y)
+        {
+            if (x < 0) x = 0;
+            if (x > size - 1) x = size - 1;
+            if (y < 0) y = 0;
+            if (y > size - 1) y = size - 1;
+            return x + y * size;
+        }
+
+        private void PositionToCoord(int position, out int x, out int y)
+        {
+            if (position < 0) position = 0;
+            if (position > (size * size - 1)) position = (size * size - 1);
+
+            x = position % size;
+            y = position / size;
+        }
+
+        //////////////////////////////////////////////////////////////////////
+        public void shift(int position)
+        {
+            int x, y;
+            PositionToCoord(position, out x, out y);
+
+            if ((Math.Abs(space_x - x) + Math.Abs(space_y - y)) != 1) return;
+
+            map[space_x, space_y] = map[x, y]; // в "нулевую" кнопку записываем нажатую кнопку
+            map[x, y] = 0;
+            space_x = x;
+            space_y = y;
+        }
+
+        public void Shuffle()
+        {
+            //shift(rand.Next(0, size * size));
+            int a = rand.Next(0, 4);
+            int x = space_x;
+            int y = space_y;
+            switch (a)
+            {
+                case 0: x--; break; // идем влево
+                case 1: x++; break; // идем вправо
+                case 2: y--; break; // up
+                case 4: y++; break; // down
+            }
+            shift(CoordToPosition(x, y));
+        }
+
+        public bool checkFinish()
+        {
+            if (!(space_x == (size - 1) && space_y == (size - 1)))
+                return false;
+
+            for (int x = 0; x < size; x++)
+                for (int y = 0; y < size; y++)
+                    if (!(x == size - 1 && y == size - 1))
+                       if (map[x, y] != (CoordToPosition(x, y) + 1))
+                          return false;
+            return true;
+        }
+    }
+}
